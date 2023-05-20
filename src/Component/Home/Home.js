1da3c420158d8b545e7a0f0ba042ab00555be631
mahesh <maheshverma.niit@gmail.com>
2023-05-20 13:02:39 +0530

    homejsx

--- src/Component/Home/Home.js
+++ src/Component/Home/Home.js
@@ -2,12 +2,8 @@ import React from 'react'
 
 export const Home = () => {
   return (
-<<<<<<< HEAD
     <div>
       <h1>home</h1>
     </div>
-=======
-    <div>Home</div>
->>>>>>> 28f0db170fa5e63894bcb54294ee3570dff2ca93
   )
 }
